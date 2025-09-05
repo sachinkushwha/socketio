d1a87015ba0a6d20acbafd8f9636ddb263205bf4
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-09-06 00:02:07 +0530

    v.0.0.5

--- client/src/components/socket.jsx
+++ client/src/components/socket.jsx
@@ -1,6 +1,6 @@
 // socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000/');
+const socket = io('https://socketio-iota.vercel.app/');
 
 export default socket;
