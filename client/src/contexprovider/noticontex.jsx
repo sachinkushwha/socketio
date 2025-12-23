1ae3bc5d82e4c3017ee429d122d9a362e53cda2a
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-12-23 14:07:33 +0530

    Update noticontex.jsx

--- client/src/contexprovider/noticontex.jsx
+++ client/src/contexprovider/noticontex.jsx
@@ -13,6 +13,8 @@ export const Statusprovider = ({ children }) => {
     const handleOnline = async (userid) => {
       setisonline(userid);
       User().then((result) => {
+        console.log(result,"result");
+        console.log(userid,"userid");
            const onlineusers=result.filter(u=>userid.includes(u._id)).map(u=>u.name);
            console.log("onlie wala",onlineusers);
             setonlineuser(onlineusers);
