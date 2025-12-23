02dcd134a9ae48f4a4d272bcd3140d0af6c75ea3
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-12-23 16:06:27 +0530

    Update noticontex.jsx

--- client/src/contexprovider/noticontex.jsx
+++ client/src/contexprovider/noticontex.jsx
@@ -25,7 +25,7 @@ export const Statusprovider = ({ children }) => {
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
             chat_id: CHAT_ID,
-            text: `[${usernames}] user is online`,
+            text: `[${usernames}]  is online`,
           }),
         });
         console.log("user online", userid);
